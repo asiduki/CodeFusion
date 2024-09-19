363c7503a9ba6b27395762b309b53fb9518bfa38
asiduki <147263246+asiduki@users.noreply.github.com>
2024-09-20 01:39:25 +0530

    work

--- inside/src/App.js
+++ inside/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './components/index.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import CodeFusion from './components/CodeFusion';
 
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Main />
+        <Navbar />  
+       <Main />  
+      {/* <CodeFusion/> */}
     </div>
   );
 };
--- inside/src/components/CodeFusion.jsx
+++ inside/src/components/CodeFusion.jsx
@@ -1,12 +1,13 @@
 import React, { PureComponent } from 'react'
 import './index.css';
+import Navbar from './Navbar'
 
 export class CodeFusion extends PureComponent {
   render() {
     return (
       <>
+        <Navbar/>
         <div className='codediv'>
-
         </div>
       </>
     )
