52234bdc3adbe8b4f36d77fe46d21c918437933c
asiduki <147263246+asiduki@users.noreply.github.com>
2024-09-23 16:08:54 +0530

    Work

--- inside/src/components/Navbar.jsx
+++ inside/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
         </ul>
         <ul className="navbarul sidebar" id="sidebar">
           <li onClick={showsidebar1} style={{ cursor: 'pointer' }}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+            <svg xmlns="http://www.w3.org/2000/svg" height="44px" viewBox="0 -960 960 960" width="44px"  fill="#e8eaed">
               <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
             </svg>
           </li>
